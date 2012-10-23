2c93c37d3132ef772d089b446fb22c37336994b1
Tomasz Kamiński <kaminski.tomasz.a@gmail.com>
2012-10-23 04:15:02 +0200

    Adding license information.
    Signed-off-by: Tomasz Kamiński <kaminski.tomasz.a@gmail.com>

--- ecj-psh/src/main/java/org/ecj/psh/PshDefaults.java
+++ ecj-psh/src/main/java/org/ecj/psh/PshDefaults.java
@@ -1,3 +1,19 @@
+/*
+* Copyright 2009-2010 Tomasz Kamiński
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package org.ecj.psh;
 
 import ec.DefaultsForm;
@@ -9,7 +25,6 @@ import ec.util.Parameter;
  * basis.
  * 
  * @author Tomasz Kamiński
- * @version 1.0
  */
 public final class PshDefaults implements DefaultsForm {
 	public static final String P_PSH = "psh";
--- ecj-psh/src/main/java/org/ecj/psh/PshEvaluator.java
+++ ecj-psh/src/main/java/org/ecj/psh/PshEvaluator.java
@@ -1,3 +1,19 @@
+/*
+* Copyright 2009-2010 Tomasz Kamiński
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package org.ecj.psh;
 
 import ec.EvolutionState;
@@ -8,7 +24,7 @@ import ec.util.Parameter;
 /**
  * Evaluator for PushGP problems. Holds interpreters for each thread in run.
  * 
- * @author Tomek
+ * @author Tomasz Kamiński
  * 
  */
 public class PshEvaluator extends SimpleEvaluator {
